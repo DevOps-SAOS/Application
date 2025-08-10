a5d92dae57a7bd9039c03ac567d850bf51146596
sivanzagdon <sivan0252@gmail.com>
2025-08-10 19:40:14 +0300

    test selenium5

--- test_pass_gen.py
+++ test_pass_gen.py
@@ -1,84 +1,91 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
 import re
-# from webdriver_manager.core.utils import ChromeType
+import time
 
-# options = webdriver.ChromeOptions()
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
+BASE_URL = "http://18.195.170.226/"
 
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
+def make_driver():
+    opts = Options()
+    for arg in [
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920,1200",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+    ]:
+        opts.add_argument(arg)
+    return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=opts)
 
-for option in options:
-    chrome_options.add_argument(option)
+def generate_and_get_answer(driver, operation_value):
+    driver.get(BASE_URL)
+    Select(driver.find_element(By.ID, "operation")).select_by_value(operation_value)
+    driver.find_element(By.ID, "generate").click()
+    time.sleep(0.2)
 
-def test_site():
-    url = "http://18.195.170.226//"    
-
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-    
-    driver.get(url)
-
-     
-    operation_select = driver.find_element(By.ID, "operation")
-    operation_select.send_keys("add")   
-    generate_button = driver.find_element(By.ID, "generate")
-    generate_button.click()
-
-     
     exercise_text = driver.find_element(By.ID, "exercise").text
-    print(f"תרגיל נוצר: {exercise_text}")
-    
-     
-    numbers = re.findall(r'\d+', exercise_text)   
-    if len(numbers) == 2:
-        num1, num2 = map(int, numbers)   
-        operator = exercise_text.split()[1]   
-    
-        if operator == "+":
-            correct_answer = num1 + num2
-        elif operator == "-":
-            correct_answer = num1 - num2
-        elif operator == "*":
-            correct_answer = num1 * num2
-        elif operator == "/":
-            correct_answer = num1 / num2
-        else:
-            print(f"לא זוהה תרגיל: {exercise_text}")
-            driver.quit()
-            return
-
-        print(correct_answer)
- 
-        answer_input = driver.find_element(By.ID, "userAnswer")
-        answer_input.send_keys(str(correct_answer))   
-        check_button = driver.find_element(By.ID, "check")
-        check_button.click()
-
-      
-        feedback = driver.find_element(By.ID, "feedback").text
-        print(f"פידבק: {feedback}")
-        print("hi")
-
-      
-        assert "נכונה" in feedback, f"פידבק לא נכנס, קיבלת: {feedback}"
-
+    nums = list(map(int, re.findall(r"\d+", exercise_text)))
+    op_symbol = None
+    for token in exercise_text.split():
+        if token in ["+", "-", "×", "÷"]:
+            op_symbol = token
+            break
+    if op_symbol == "+":
+        answer = nums[0] + nums[1]
+    elif op_symbol == "-":
+        answer = nums[0] - nums[1]
+    elif op_symbol == "×":
+        answer = nums[0] * nums[1]
+    elif op_symbol == "÷":
+        answer = nums[0] // nums[1]
     else:
-        print(f"לא נמצא תרגיל תקני: {exercise_text}")
-    
-    
-    driver.quit()
+        raise AssertionError(f"אופרטור לא מזוהה: {op_symbol}")
+    return answer
+
+def submit_answer(driver, value):
+    answer_input = driver.find_element(By.ID, "userAnswer")
+    answer_input.clear()
+    answer_input.send_keys(str(value))
+    driver.find_element(By.ID, "check").click()
+    time.sleep(0.2)
+    return driver.find_element(By.ID, "feedback").text
+ 
+def test_add_correct():
+    d = make_driver()
+    try:
+        correct_answer = generate_and_get_answer(d, "add")
+        feedback = submit_answer(d, correct_answer)
+        assert "תשובה נכונה" in feedback, f"ציפיתי ל'תשובה נכונה', קיבלתי: {feedback}"
+    finally:
+        d.quit()
 
  
-test_site()
+def test_add_incorrect():
+    d = make_driver()
+    try:
+        correct_answer = generate_and_get_answer(d, "add")
+        feedback = submit_answer(d, correct_answer + 1)
+        assert "לא נכון" in feedback, f"ציפיתי ל'לא נכון', קיבלתי: {feedback}"
+    finally:
+        d.quit()
+
+ 
+def test_multiply_correct():
+    d = make_driver()
+    try:
+        correct_answer = generate_and_get_answer(d, "mul")
+        feedback = submit_answer(d, correct_answer)
+        assert "תשובה נכונה" in feedback, f"ציפיתי ל'תשובה נכונה', קיבלתי: {feedback}"
+    finally:
+        d.quit()
+
+if __name__ == "__main__":
+    test_add_correct()
+    test_add_incorrect()
+    test_multiply_correct()
